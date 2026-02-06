6273ed0524e18e02add669f672bd9b77e637709d
manishaSingh8482 <manishasingh49889@gmail.com>
2026-02-07 00:36:20 +0530

    Update main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.0.2.0/24"
+}
